f30052b2f86118ee2c19f70eb4c518e7a8714e38
IamThinn <thien86087@st.vimaru.edu.vn>
2024-05-15 08:05:52 +0700

    Update db.php

--- users/db.php
+++ users/db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect("thiensql.mysql.database.azure.com","thiensql","123999Aa@","");
+    $con = mysqli_connect("thiensql.mysql.database.azure.com","thiensql","123999Aa@","kapetann");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
